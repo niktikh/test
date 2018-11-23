d52706841fbd8367f4428853a716224983ee9a76
nikita tikh <nikits93@mail.ru>
2018-11-23 23:56:42 +0300

    Add work with input in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var main = function () {
     $(".comment-input button").on("click", function (event) {
-        console.log("Hello, World!");
+       var $newComment = $("<p>");
+       $newComment.text($(".comment-input input").val());
+        $(".comments").append($newComment);
     });
 };
 $(document).ready(main);
