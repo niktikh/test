a464221f57e57051cee03c29e2fca151b11ae053
nikita tikh <nikits93@mail.ru>
2018-11-25 17:02:44 +0300

    update app

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var main = function () {
+    var addCommentFromInputBox = function () {
+        var $newComment = $("<p>");
+        if ($(".comment-input input").val() !== "") {
+            $newComment.text($(".comment-input input").val());
+            $newComment.hide();
+            $(".comments").append($newComment);
+            $newComment.fadeIn();
+            $(".comment-input input").val("");
+        }
+    }
     $(".comment-input button").on("click", function (event) {
-       var $newComment = $("<p>");
-       $newComment.text($(".comment-input input").val());
-        $(".comments").append($newComment);
-    });
-};
+        addCommentFromInputBox()
+    })
+    $(".comment-input input").on("keypress", function (event) {
+        if (event.keyCode === 13) {
+            addCommentFromInputBox()
+        }
+    })
+}
 $(document).ready(main);
