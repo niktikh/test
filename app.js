306cb50097d8930c2816429ff1c001c028d00a39
nikita tikh <nikits93@mail.ru>
2018-11-26 20:44:59 +0300

    update app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var main = function () {
             $newComment.fadeIn();
             $(".comment-input input").val("");
         }
-    }
+    };
     $(".comment-input button").on("click", function (event) {
         addCommentFromInputBox()
     })
@@ -16,6 +16,42 @@ var main = function () {
         if (event.keyCode === 13) {
             addCommentFromInputBox()
         }
-    })
-}
+    });
+    $(".comments p").on("click",function (event) {
+        $(".comments p").fadeOut(1000, function () {
+            $(".comments p").remove();
+        });
+    });
+    var toDos = [
+        "Закончить писать эту книгу",
+        "Вывести Грейси на прогулку в парк",
+        "Ответить на электронные письма",
+        "Подготовиться к лекции в понедельник",
+        "Обновить несколько новых задач",
+        "Купить продукты"
+    ];
+    //... все остальное, относящееся к вкладкам
+
+
+    $(".tabs span").toArray().forEach(function (element) {
+        $(element).on("click", function() {
+            var $element = $(element);
+            // $cont;
+            $(".tabs span").removeClass("active");
+            $element.addClass("active");
+            $("main .comments").empty();
+            if ($element.parent().is(":nth-child(1)")) {
+                console.log("Щелчок на первой вкладке!");
+            } else if ($element.parent().is(":nth-child(2)")) {
+                // $cont=$("p");
+                toDos.forEach(function (TODO) {
+                    $(".comments").append($("<p>").text(TODO));
+                });
+            } else if ($element.parent().is(":nth-child(3)")) {
+                console.log("Щелчок на третьей вкладке!");
+            }
+            return false;
+        });
+    });
+};
 $(document).ready(main);
